29baf5d9e6e10a58125286a09a98a9e554343b44
UNFlappable <harunyoney@gmail.com>
2024-01-19 13:22:16 +0300

    vki

---
+++ JavaScript Practise/vki.js
@@ -0,0 +1,24 @@
+a=5
+console.log(a);
+
+
+
+let boy = Number(prompt("lütfen boy giriniz x.xx şeklinde"))
+console.log(typeof boy);
+console.log( boy);
+let kilo= Number(prompt("lütfen kilonuzu giriniz" ))
+console.log(typeof kilo);
+
+let vki = kilo/(boy**2)
+console.log(vki);
+
+
+
+
+ 
+    if (vki<= 18,5) {console.log("altında" );}
+    if (18,5<vki<=25) {console.log("ideal");}
+    if (25<vki<=29,9) {console.log("kilolu");}
+    if (vki>=30)  {console.log("obez");}
+    // {console.log("tanımsız");}
+    
