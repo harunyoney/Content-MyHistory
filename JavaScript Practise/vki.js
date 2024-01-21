2708fe38a5a3e1a73155588dca221129ff088331
UNFlappable <harunyoney@gmail.com>
2024-01-21 18:02:32 +0300

    JavaScript practise

--- JavaScript Practise/vki.js
+++ JavaScript Practise/vki.js
@@ -1,36 +1,36 @@
-a=10 / 0
-let x =5;
-const y =3;
-let xy =x += y
-console.log(xy);
-console.log(10 / 3);
-console.log(typeof null);
-console.log("true" == true);
-console.log(typeof a);
-console.log( 5*"2");
+// a=10 / 0
+// let x =5;
+// const y =3;
+// let xy =x += y
+// console.log(xy);
+// console.log(10 / 3);
+// console.log(typeof null);
+// console.log("true" == true);
+// console.log(typeof a);
+// console.log( 5*"2");
 
-console.log(typeof "5" + 5);
-console.log(5 + "5");
+// console.log(typeof "5" + 5);
+// console.log(5 + "5");
 
-// let boy = Number(prompt("lütfen boy giriniz x.xx şeklinde"))
-// console.log(typeof boy);
-// console.log( boy);
-// let kilo= Number(prompt("lütfen kilonuzu giriniz" ))
-// console.log(typeof kilo);
+let boy = Number(prompt("lütfen boy giriniz x.xx şeklinde"))
+console.log(typeof boy);
+console.log( boy);
+let kilo= Number(prompt("lütfen kilonuzu giriniz" ))
+console.log(typeof kilo);
 
-// let vki = kilo/(boy**2)
-// console.log(vki);
+let vki = kilo/(boy**2)
+console.log(vki);
 
 
 
 
  
-//     if (vki<= 18,5) {console.log("altında" );}
-//     if (18,5<vki<=25) {console.log("ideal");}
-//     if (25<vki<=29,9) {console.log("kilolu");}
-//     if (vki>=30)  {console.log("obez");}
-//     // {console.log("tanımsız");}
+    if (18,5>=vki) {console.log(vki , "altında" );}
+    else if (25>=vki) {console.log(vki ,"ideal");}
+    else if (29,9>=vki) {console.log(vki ,"kilolu");}
+    else   {console.log("vki" ,vki ,"obez");}
+    // {console.log("tanımsız");}
     
-// console.log(5 +"5");
+console.log(5 +"5");
 
 
