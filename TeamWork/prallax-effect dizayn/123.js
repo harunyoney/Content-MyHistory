989f60fc1f0380a8d9e9ae0fa2f4b92af5929839
UNFlappable <harunyoney@gmail.com>
2024-01-25 13:44:28 +0300

    Prallaxx

---
+++ TeamWork/prallax-effect dizayn/123.js
@@ -0,0 +1,54 @@
+/* 
+
+// 0 ile 100 arası n sayıda rasgele tam sayıda
+// for ile 
+
+
+// let sayı = +prompt("lütfen 0-100 arası bir sayı girin ",50 )
+
+
+
+// for ( let i = 1   ; i <= 100  ; i++  ) {
+//     let x = Math.trunc(Math.random()*100 )
+//     console.log(x);
+// }
+
+
+
+// for ( let i =    ; i <= 10  ; 2  ) {
+     
+//     console.log(i);
+// }
+
+
+
+let sum = 0;
+let number =0 ;
+
+ */
+
+// console.log("***** FOR *****");
+
+// let y=0
+// let i =0
+// for (i ; i<2 ; i++) {
+//     let x =+prompt(`${i}.note`)
+
+//     y+=x
+//     console.log(y);
+    
+    
+// }
+// console.log(`avarage= ${y/i}`);
+
+
+let n1=15
+let n2=25
+let sum=0
+
+for (n1 ; n1<=n2 ; n1++){
+    sum+=n1
+    console.log(sum);
+}
+
+
