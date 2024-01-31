c850dfcbda788d90298bb05d0161377c274314de
UNFlappable <harunyoney@gmail.com>
2024-01-31 11:43:19 +0300

    guessgame is finished

--- practise/loop practise/guessGame.js
+++ practise/loop practise/guessGame.js
@@ -1,3 +1,36 @@
 //* Program 0-100 arasında rasgele bir sayi tutacak ve kullanicinin bu sayiyi 5 kere(hak) de bilmesini isteyecektir. Her yanlista hakkini bir dusurecek ve ARTTIR/AZALT diyerek kullaniciyi yonlendirecektir.Sonuc olarak kullanicinin hakki 0 olursa "Uzgunuz kaybettiniz" eger bildi ise "Tebrikler ... denemede bildiniz" yazacaktir.
 
 console.log("*** WELCOME TO GUESS GAME ****")
+
+let roll= Math.round(Math.random()*100)
+let trial=+prompt("lütfen kaç deneme yapmak istediğinizi seçimiz")
+let guess=""
+
+do {
+    let guess=+prompt("tahmini giriniz")
+    console.log(roll);
+
+    if(guess===roll) {
+        console.log("tebrikler cok iyisin");
+        break;
+    }else if(guess>roll){
+        trial-=1
+        alert(`daha küçük at
+         kalan deneme sayisi=${trial}`);
+    }else if(guess<roll){
+        trial-=1
+        alert(`daha büyük at
+         kalan deneme sayisi=${trial}`);
+    }
+    console.log(trial);
+
+
+    
+} 
+
+while (trial>0);
+if (guess !== roll) {alert("üzgünüm elendiniz")}
+
+
+
+
---
+++ practise/loop practise/reverse.js
@@ -0,0 +1,76 @@
+
+
+
+
+// let reversedSentence = "";
+// let sentence = prompt("yazı yaz")
+
+
+    
+    
+    
+//     let word = "";
+    
+//     for(let i = sentence.length -1; i >= 0 ; i--) {
+     
+//       if(sentence[i] === " " || i === 0 ) {
+       
+//         if(i === 0) {
+//           word += sentence[i]; 
+//         }
+        
+//         for (let j = word.length - 1; j >= 0; j--) {
+//           reservedSentence += word[j] ;
+//         }
+        
+//         if( i !== 0 ) {
+//           reversedSentence += " ";
+//         }
+        
+//         word = "";
+//       } else {
+        
+//         word += sentence[i];
+//       }
+//     }
+    
+  
+
+// console.log(reversedsentence) ;
+
+
+
+// BURASI YENİ YER
+
+  
+// let x ="yazi yazia"
+// let i =x.length -1
+// console.log(i);
+// for(let i =x.length -1 ; i>=0 ; i--) {
+//     if(x[i]===" " || i ===0)
+// }
+
+
+
+
+// let y=0
+// let x =0
+// for (i =0;   ;i++ )
+
+
+
+let n = 5
+if (n===1) {
+    console.log("0");
+
+}else if (n===2) {
+    console.log("1");
+}else {
+    let a=0, b=1;
+    let cikti ="0, 1"
+    
+
+
+    
+}
+
