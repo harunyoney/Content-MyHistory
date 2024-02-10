46b84dd946c438417c97427674eeb995a069a3c0
UNFlappable <harunyoney@gmail.com>
2024-02-10 21:10:16 +0300

    animation

---
+++ TeamWork/Turgay çalışma/abc.js
@@ -0,0 +1,11 @@
+function iterateString(inputString){
+    //write your code here
+    
+    for (let i=0 ; i<str.length;i++ ){
+        console.log(` harf ${str[i]}`);
+    }
+
+
+    }
+    let str = 'Hello World';
+    iterateString(str)
