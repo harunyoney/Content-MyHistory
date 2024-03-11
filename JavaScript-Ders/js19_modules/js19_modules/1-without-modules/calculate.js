365b650f2fe98f5096519309828d8c8903e7f8a4
UNFlappable <harunyoney@gmail.com>
2024-03-11 22:09:53 +0300

    modules

--- JavaScript-Ders/js19_modules/js19_modules/1-without-modules/calculate.js
+++ JavaScript-Ders/js19_modules/js19_modules/1-without-modules/calculate.js
@@ -1,1 +1,15 @@
+// "use strict" //! Derleyici direktifi
+
 console.log("CALCULATE.JS")
+
+const totalPrice = (products) => {
+  return products.reduce((sum, product) => sum + product.price, 0)
+}
+
+//? Global namespace
+// const students = ["Canan", "Can"]
+
+myVariable = "my string" //! use Strict -> myVariable is not defined
+console.log(myVariable)
+
+console.log(this) //? window
--- JavaScript-Ders/js19_modules/js19_modules/2-browser-es-modules/app.js
+++ JavaScript-Ders/js19_modules/js19_modules/2-browser-es-modules/app.js
@@ -6,3 +6,10 @@
 //? JS kodlarının kolay ve performansli bir şekilde farklı dosyalara ayrilmasina ve gerektiginde birleştirmesine olanak saglamaktadir.
 //? Bir degisken,dizi,obje, fonksiyon, class v.b "export" deyimi ile başka dosyalarinin kullanimina açilmabilir.
 //? Kullanima acilanlar ise import deyimi ile istenilen dosyaya eklenebilir.
+import { add } from "./hesapMakinasi.js"
+import { sub, mul, div } from "./hesapMakinasi.js"
+
+console.log("ADD:", add(3, 5))
+console.log("SUB:", sub(3, 5))
+console.log("MUL:", mul(6, 5))
+console.log("DIV:", div(8, 5))
