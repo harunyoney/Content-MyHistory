2350928d015caa0dfe0a5d4bd2023647eb2b5f36
UNFlappable <harunyoney@gmail.com>
2024-03-26 00:19:30 +0300

    state

--- js-React-Ders/events-useStateHook/src/2-useStateComp/UseStateObject.jsx
+++ js-React-Ders/events-useStateHook/src/2-useStateComp/UseStateObject.jsx
@@ -1,19 +1,64 @@
+import { useState } from "react"
+
 const UseStateObject = () => {
+  //? State tanımlamaları
+  //   const [ad, setAd] = useState("Mehmet")
+  //   const [soyad, setSoyad] = useState("Yılmaz")
+  //   const [meslek, setMeslek] = useState("Tester")
+  //   const [salary, setSalary] = useState(50000)
+
+  //   let toggle = true
+  const [kisi, setKisi] = useState({
+    ad: "Mehmet",
+    soyad: "Yılmaz",
+    meslek: "Tester",
+    salary: 50000,
+    mood: "Happy",
+  })
+
+  const [toggle, setToggle] = useState(true)
+
+  //   const adDegistir = () => {
+  //     setAd("Ahmet")
+  //   }
+
+  console.log(kisi)
+
+  const handleMood = () => {
+    setKisi({ ...kisi, mood: toggle ? "Sad" : "Happy" })
+    setToggle(!toggle)
+    // toggle = !toggle
+  }
+
+  console.log(toggle)
   return (
-    <div>
+    <>
       <h1>UseState Object Example</h1>
 
-      <h2>AD:ad</h2>
-      <h2>SOYAD:soyad</h2>
-      <h2>MESLEK:meslek</h2>
-      <h2>MAAS:maas</h2>
+      <h2>AD:{kisi.ad}</h2>
+      <h2>SOYAD:{kisi.soyad}</h2>
+      <h2>MESLEK:{kisi.meslek}</h2>
+      <h2>MAAS:{kisi.salary}</h2>
+      <h3>Mood:{kisi.mood} </h3>
 
-      <button>ad degistir</button>
-      <button>soyad degistir</button>
-      <button>meslek degistir</button>
-      <button>maaas artir</button>
-      <button>maaas azalt</button>
-    </div>
+      <button onClick={() => setKisi({ ...kisi, ad: "Ahmet" })}>
+        ad degistir
+      </button>
+      <button onClick={() => setKisi({ ...kisi, soyad: "Ozturk" })}>
+        soyad degistir
+      </button>
+      <button onClick={() => setKisi({ ...kisi, meslek: "Developer" })}>
+        meslek degistir
+      </button>
+      <button onClick={() => setKisi({ ...kisi, salary: kisi.salary + 5000 })}>
+        maaas artir
+      </button>
+      <button onClick={() => setKisi({ ...kisi, salary: kisi.salary - 1000 })}>
+        maaas azalt
+      </button>
+
+      <button onClick={handleMood}>Happy/Sad</button>
+    </>
   )
 }
 
--- js-React-Ders/events-useStateHook/src/App.js
+++ js-React-Ders/events-useStateHook/src/App.js
@@ -1,11 +1,13 @@
 import Event from "./1-events/Event";
 import UseStateCounter from "./2-useStateComp/UseStateCounter";
+import UseStateObject from "./2-useStateComp/UseStateObject";
 
 function App() {
   return (
     <>
       <Event />
       <UseStateCounter />
+      <UseStateObject />
     </>
   );
 }
