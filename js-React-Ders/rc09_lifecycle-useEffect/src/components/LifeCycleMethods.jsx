c5abb7602835248e7a539c4d0ddc3b88aadcce4b
UNFlappable <harunyoney@gmail.com>
2024-04-03 21:49:54 +0300

    life cycle

---
+++ js-React-Ders/rc09_lifecycle-useEffect/src/components/LifeCycleMethods.jsx
@@ -0,0 +1,48 @@
+//?=============================================================
+//?                 LIFECYCLE METOTLARI
+//?      https://reactjs.org/docs/react-component.html
+//?=============================================================
+
+//* Lifecycle metotlari componentlerin DOM'da varoldugu sure boyunca
+//* uzerinde islem yapmamizi imkan saglayan ozel React metotlaridir.
+//* Ornegin bir component olusturuldugunda, DOM'a basildiginda,
+//* guncellendiginde veya DOM'dan kaldirildiginda bir seyler yapmak icin
+//* lifecycle metotlari kullanilabilir.
+//* En bilindik lifecycle metodu render() metodudur
+
+//* Bir component'in olusturulmasi (constructor),
+//* Bir componentin DOM agacina eklenmesinin hemen sonrasi(componentDidMount)
+//* Bir component'in DOM'a basilmasi (render)
+//* (Optional) Bir componentin guncellenmesinin hemen sonrasi (componentDidUpdate)
+//* Bir component'in DOM agacindan kaldirilmasi sonrasi(componentWillUnmount)
+
+import React from "react"
+
+class LifeCycleMethods extends React.Component {
+  constructor(props) {
+    console.log("Constructor running")
+    super(props)
+    this.state = {
+      count: 0,
+    }
+  }
+
+  handleInc = () => {
+    this.setState({
+      count: this.state.count + 1,
+    })
+  }
+
+  render() {
+    return (
+      <div className="container text-center">
+        <h1 className="text-danger">LIFECYCLE METHODS</h1>
+        <h3>COUNT={this.state.count}</h3>
+        <button className="btn btn-info" onClick={this.handleInc}>
+          INC
+        </button>
+      </div>
+    )
+  }
+}
+export default LifeCycleMethods
