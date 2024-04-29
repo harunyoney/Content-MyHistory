3cdecb0ac7a86af907987db22dc22409da5f52f3
UNFlappable <harunyoney@gmail.com>
2024-04-29 20:36:44 +0300

    redux

--- js-React-Ders/rc13_tailwind/src/App.js
+++ js-React-Ders/rc13_tailwind/src/App.js
@@ -1,7 +1,20 @@
+import Typografy from "./components/Typografy";
+
 function App() {
   return (
     <div className="App">
       <h2 className="text-purple-700">Hoşgeldin Ya Şehri React</h2>
+      <h6 className="text-xs">H6 OLDU</h6>
+      <h1 className="text-2xl">H1 OLDU</h1>
+      <h1 className="text-[3rem]">H1 OLDU</h1>
+      <Typografy />
+
+      <h1 className="line-clamp-1">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus sed vel
+        porro pariatur voluptatibus et quasi neque, ipsa ex atque repellat eaque
+        sunt suscipit magnam maxime. Iure magnam optio, enim cumque totam
+        quaerat.
+      </h1>
     </div>
   );
 }
---
+++ js-React-Ders/rc13_tailwind/src/components/Typografy.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Typografy = () => {
+  return (
+    <div>Typografy</div>
+  )
+}
+
+export default Typografy
